54f884277685f86bb315af20a52cc8b369359876
samwathegreat <4813336+samwathegreat@users.noreply.github.com>
2026-09-25 22:36:34 -0400

    Create META-INF before writing theme version metadata

--- lib/theme.sh
+++ lib/theme.sh
@@ -25,7 +25,7 @@ build_theme()(
   work="$(mktemp -d)"
   trap 'rm -rf "${work:-}"' EXIT
   asset_base="${AVAGATO_ASSET_BASE:-https://raw.githubusercontent.com/samwathegreat/avagato/main/theme/assets}"
-  install -d "$work/resources/images" "$work/translations"
+  install -d "$work/META-INF" "$work/resources/images" "$work/translations"
 
   curl -fL --retry 3 --proto '=https' --tlsv1.2 "$asset_base/avagato-login.png" -o "$work/resources/images/avagato-login.png"
   curl -fL --retry 3 --proto '=https' --tlsv1.2 "$asset_base/avagato-large.png" -o "$work/resources/images/avagato-large.png"
